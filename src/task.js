2eade94b4507b2dd6a9035f463d6444b9a2c9f04
Alondra <tornit7.8@gmail.com>
2024-07-30 14:20:02 -0500

    Estructura HTML, estilos CSS, inicio de JS

---
+++ src/task.js
@@ -0,0 +1,11 @@
+// Obtener la lista de tareas del almacenamiento del navegador "localStorage"
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Función para agregar una tarea a la lista de tareas 
+export const addTask = (task) => {
+    const newTask = {
+        id: Date.now(),
+        text: task,
+        completed: false,
+    }; 
+}
